de897825b5a3699e7a980b2c4ecdbd072e8998f5
CROSS\jsiddi <jerome.siddi@microej.com>
2019-02-22 15:44:53 +0100

    M0018PQT-3 add coremark entrypoint

--- tests/core/c/inc/t_core_core_benchmark.h
+++ tests/core/c/inc/t_core_core_benchmark.h
@@ -4,14 +4,14 @@
  * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
+
 /* Prevent recursive inclusion */
 
-#ifndef __T_CORE_RAM_H
-#define __T_CORE_RAM_H
+#ifndef __T_CORE_CORE_BENCHMARK_H
+#define __T_CORE_CORE_BENCHMARK_H
 #include <embUnit/embUnit.h>
 
 /** Public function declarations */
-TestRef  T_CORE_RAM_tests(void);
-TestRef  T_CORE_RAM_speed_tests(void);
+TestRef T_CORE_COREBENCH_tests(void);
 
 #endif
---
+++ tests/core/c/inc/x_core_benchmark.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019 IS2T. All rights reserved.
+ * For demonstration purpose only.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/* Prevent recursive inclusion */
+
+#ifndef __X_CORE_BENCHMARK_H
+#define __X_CORE_BENCHMARK_H_H
+#include <stdint.h>
+#include <stdbool.h>
+
+/** Public constant declaration */
+
+
+
+/** public structure declaration */
+
+
+/** public function declaration */
+// weak function to implement
+bool X_CORE_BENCHMARK_run(void);
+#endif
