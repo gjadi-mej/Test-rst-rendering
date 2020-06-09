396f1fc802b87b6ff67699475e81b19c1133ca26
Benjamin Alix <benjamin.alix@microej.com>
2020-06-09 16:53:32 +0200

    M0018PQT-61 Remove C JVM validation

--- tests/core/c/src/x_mjvm_port_validation.c
+++
@@ -1,9 +0,0 @@
-/*
- * C
- *
- * Copyright 2016-2020 MicroEJ Corp. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be found with this software.
- */
-#include "sni.h"
-jfloat Java_com_microej_MicroejCoreValidation_testFloat (jfloat a, jfloat b) {return a * b;}
-jdouble Java_com_microej_MicroejCoreValidation_testDouble (jdouble a, jdouble b) {return a * b;}
